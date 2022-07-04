516e3e57d3fa2ffed561a041bbe0b10571003fd9
meltsplit <seokwoo2000@naver.com>
2022-07-05 02:58:24 +0900

    Initial Commit

---
+++ Clone_MangoPlate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clone_MangoPlate
+//
+//  Created by 장석우 on 2022/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
